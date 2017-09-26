348696e7ec6489727691d56a446971cea3710bb8
versachin <31958628+versachin@users.noreply.github.com>
2017-09-26 17:44:47 -0400

    Chapter 3 Exercise Problem 1

---
+++ Ch. 3/Chaoter 3 Exercises Problem 2.py
@@ -0,0 +1,4 @@
+months = ["Jan", "Feb", "March"]
+
+for i in months:
+    print("One of the months of the year is " + i)
---
+++ Ch. 3/Chapter 3 Exercise 1 Problem 1.py
@@ -0,0 +1,5 @@
+sentence= "We like Python's turtles!"
+
+for i in range (1000):
+    print(sentence)
+    
